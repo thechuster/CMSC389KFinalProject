7313cdf6f7e92a753719f71f234029d186805297
tsiporastone <tsipora.stone@gmail.com>
2019-11-19 22:33:59 -0500

    some mongodb and schemas

---
+++ Album.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        min: 0.0,
+        max: 5.0,
+        required: true
+    },
+    comment: {
+        type: String
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+});
+
+var albumSchema = new mongoose.Schema({
+    artist: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        min: 0,
+        max: 2019,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    PicURL: {
+        type: String
+    },
+    reviews: [reviewSchema]
+});
+
+var Album = mongoose.model('Album', albumSchema);
+module.exports = Album;
